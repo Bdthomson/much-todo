e8e41616ae7a85f585e7d93257739222a0058838
Blake Thomson <blake.thomson@connexta.com>
2018-10-29 17:49:44 -0700

    blake-todo Work in progress

--- vanilla/src/index.js
+++ vanilla/src/index.js
@@ -6,61 +6,135 @@ var todos = [
     { id: 3, text: "item 3", completed: true }
 ];
 
-document.getElementById('todoForm').onsubmit = function (e) {
-    e.preventDefault();
 
-    var text = document.getElementById('todoText').value;
-    todos.push({ id: guid(), text: text, completed: false });
+document.getElementById('todoText').addEventListener("keyup", function(e){
+    e.preventDefault()
+    if (event.keyCode === 13) {
+        document.getElementById("addButton").click();
+      }
+})
+document.getElementById('addButton').onclick = addTodo
+document.getElementById('completeButton').onclick = completeAllTodos
+document.getElementById('uncompleteButton').onclick = uncompleteAllTodos
 
-    console.log("Todos: ", todos)
-    var text = document.getElementById('todoText').value = "";
+function getNumTodosLeft() {
+    return todos.filter(t => !t.completed).length
+}
+
+function completeAllTodos() {
+    console.log("Completed")
+    todos.forEach(todo => todo.completed = true)
     updateTodos()
 }
 
-function getTodoItem(todo, checked) {
-    console.log("Todo Text: ", todoText)
-    return `
-        <input type="checkbox" id="${todo.id}" onClick="toggleDone()" ${checked ? 'checked' : ''}>
-        &nbsp; <span>${todo.text}</span>
-    `
+function uncompleteAllTodos() {
+    console.log("Uncompleted")
+    todos.forEach(todo => todo.completed = false)
+    updateTodos()
+}
+
+function deleteTodo(e) {
+    var id = e.target.id
+    console.log("Called delete todo with id: ", id)
+    todos = todos.filter(t => t.id.toString() !== id)
+    console.log(todos)
+    updateTodos()
+}
+
+function updateTodo(e) {
+    var id = e.target.id
+    console.log("Called update todo with id: ", id)
+    todos = todos.filter(t => t.id.toString() !== id)
+    console.log(todos)
+    updateTodos()
+}
+
+function addTodo(e) {
+    e.preventDefault()
+    var text = document.getElementById('todoText').value;
+    if (text !== "") {
+        todos.push({ id: guid(), text: text, completed: false });
+        var text = document.getElementById('todoText').value = "";
+        updateTodos()
+    }
+}
+
+function toggleTodo(e) {
+    var id = e.target.id
+    var todo = todos.find(t => t.id.toString() === id)
+    todo.completed = !todo.completed
+    document.getElementById(id).checked = todo.completed
+    updateTodos()
+}
+
+function getTodoItem(todo) {
+    var todoDisplay = document.createElement('div')
+    var text = document.createElement('span')
+    text.innerHTML = " " + todo.text
+    var inputBox = h('input', {
+        id: todo.id,
+        type: 'checkbox',
+        style: "margin: 10px;"
+    }, [])
+
+    inputBox.onclick = toggleTodo
+    inputBox.checked = todo.completed
+
+    var deleteButton = h('button', {
+        id: todo.id,
+        style: "margin: 10px"
+    }, [])
+
+    deleteButton.onclick = deleteTodo
+    deleteButton.className = 'btn btn-danger'
+    deleteButton.innerHTML = " Delete"
+
+    todoDisplay.appendChild(inputBox)
+    todoDisplay.appendChild(text)
+    todoDisplay.appendChild(deleteButton)
+
+    return todoDisplay
+}
+
+const h = (tag, props, children) => {
+    var el = document.createElement(tag)
+    Object.keys(props).forEach(key => {
+        el.setAttribute(key, props[key])
+    })
+
+    if (!Array.isArray(children)) {
+        children = [children]
+    }
+    children.forEach(child => el.appendChild(child))
+
+    return el
 }
 
 function getTodoItems(todos) {
     return todos
         .filter(t => !t.completed)
-        .map(t => {
-            return `
-                <div style="margin-top:10px">
-                    ${getTodoItem(t, false)}
-                </div>
-            `
-        }).join('')
+        .map(t => h('div', { style: "marginTop:10px" }, getTodoItem(t)))
 }
-
-//TODO: Make this use ids instead of text
-function toggleDone(todoText) {
-    todos = todos.filter(t => t == todoText);
-    updateTodos()
-}
-
 function getCompletedTodoItems(todos) {
-    console.log("TODOS: ", todos)
     return todos
         .filter(t => t.completed)
-        .map(t => {
-            return `
-                <div style="margin-top:10px">
-                    ${getTodoItem(t, true)}
-                </div>
-            `
-        })
+        .map(t => h('div', { style: "marginTop:10px" }, getTodoItem(t)))
 }
 
 function updateTodos() {
-    var todoHtml = getTodoItems(todos);
-    console.log("TODO HTML: ", todoHtml)
-    document.getElementById('todoItems').innerHTML = todoHtml
-    document.getElementById('completedItems').innerHTML = getCompletedTodoItems(todos)
+    var todoSelector = document.getElementById('todoItems')
+    var todoItems = getTodoItems(todos);
+    todoSelector.innerHTML = ""
+    todoSelector.appendChild(h('div', {}, todoItems))
+
+    var completedTodoSelector = document.getElementById('completedItems')
+    var completedTodoItems = getCompletedTodoItems(todos);
+    completedTodoSelector.innerHTML = ""
+    completedTodoSelector.appendChild(h('div', {}, completedTodoItems))
+
+    document.getElementById('numTodosLeft').innerHTML = "(" + getNumTodosLeft() + ")"
+    document.getElementById('numTodosComplete').innerHTML = "(" + (todos.length - getNumTodosLeft()) + ")"
+    console.log(todos)
 }
 
 updateTodos()
@@ -73,9 +147,3 @@ function guid() {
     }
     return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
-
-
-// function onSubmit() {
-//     var todoText = document.getElementById("todoText").value;
-//     console.log("Submitted with: ", todoText)
-// }
